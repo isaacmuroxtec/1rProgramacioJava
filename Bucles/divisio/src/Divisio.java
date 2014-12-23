d1f68ba650edd8e73afaf65ee1f089a7177db504
Isaac Muro <willezumleben@gmail.com>
2014-12-23 22:39:31 +0100

    Calcul de la divisió entera del dividend pel divisor.

---
+++ Bucles/divisio/src/Divisio.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Divisio {
+	public static void main(String[] args) {
+		Scanner lector = new Scanner (System.in);
+		System.out.println("Introdueix el dividend:");
+		int dividend = lector.nextInt();
+		
+		System.out.println("Introdueix el divisor:");
+		int divisor = lector.nextInt();
+		
+		int i = 0;
+		while (divisor <= dividend){
+			dividend = dividend - divisor;
+			i = i + 1;
+		}
+		
+		System.out.println("El residu es:"+dividend);
+		System.out.println("El quocient es:"+i);
+		
+	}
+}
