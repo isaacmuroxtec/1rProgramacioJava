8943910bc63964cee0befb07aac33c0b070a9d80
Isaac Muro <willezumleben@gmail.com>
2014-12-23 22:22:07 +0100

    Taula de multiplicar fins el 10.

---
+++ Bucles/taulesmultiplicar/src/Taules.java
@@ -0,0 +1,26 @@
+
+public class Taules {
+
+	public static void main(String[] args) {
+		// No demanem res.
+		int i = 0;
+		int j = 0;
+		while (j <= 10){
+			
+			i = 0; 
+			while (i <= 10){
+				System.out.println(j+"x"+i+"="+i*j);
+				
+				i = i + 1;
+			}	
+
+			j = j + 1;
+		}
+		
+		
+		
+		
+
+	}
+
+}
