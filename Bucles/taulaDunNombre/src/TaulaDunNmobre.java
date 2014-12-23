f5f3b900faf73d757a36cfab1bc26b39d2a36fd0
Isaac Muro <willezumleben@gmail.com>
2014-12-23 22:24:00 +0100

    Taula de Multiplicar demanant un nombre

---
+++ Bucles/taulaDunNombre/src/TaulaDunNmobre.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class TaulaDunNmobre {
+	public static void main(String[] args) {
+		
+		Scanner lector = new Scanner(System.in);
+		System.out.println("Introdueix un nombre:");
+		int taula = lector.nextInt();
+		
+		// Valor inicial de la i
+		int i = 10;
+		System.out.println("La taula es:");
+		
+		while (i >= 0){
+			// Feina del bucle
+			int resultat = taula * i;
+			System.out.println(taula + "x" + i + "=" + resultat);
+		
+			i =  i - 1;
+		}
+
+	}
+
+}
+
+
+
+
+
