cde288015eaa165ebac8f56ff6f1b574a92a7b67
Isaac Muro <willezumleben@gmail.com>
2014-12-24 12:08:56 +0100

    Primer bucle. Mostra tants guions com voltes del bucle.

---
+++ Bucles/bucle/src/Bucle.java
@@ -0,0 +1,19 @@
+
+public class Bucle {
+
+	public static void main(String[] args) {
+		
+		// Declara una variable de control
+		int i = 0;
+		
+		// condicio de !final
+		while (i < 10){
+			System.out.print("-");
+			
+			// Increment de la variable de control
+			i = i + 1;
+		}
+
+	}
+
+}
